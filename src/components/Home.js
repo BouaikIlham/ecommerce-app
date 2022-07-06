6b2bd55a29c722c79be90046db4b3aea46059ea1
ilham bouaik <ibouaik@gmail.com>
2022-07-06 22:28:35 +0100

    add product compenents and fech data

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,12 @@ import image from '../images/image.jpg'
 const Home = () => {
     return (
         <div className="hero">
-            <div class="card bg-dark text-white border-0">
-                <img src={image} class="card-img" alt="Background" height="550px" />
-                <div class="card-img-overlay d-flex flex-column justify-content-center">
+            <div className="card bg-dark text-white border-0">
+                <img src={image} className="card-img" alt="Background" height="550px" />
+                <div className="card-img-overlay d-flex flex-column justify-content-center">
                     <div className="container">
-                        <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
-                        <p class="card-text lead fs-2">CHECK ALL THE TRENDS</p>
+                        <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
+                        <p className="card-text lead fs-2">CHECK ALL THE TRENDS</p>
                     </div>
 
                 </div>
